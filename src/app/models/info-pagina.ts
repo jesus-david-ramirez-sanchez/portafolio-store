9720aa3802cdebaba16162b84a8d1d17128ac037
jesus david <jesus.ramirez@aossas.com>
2019-02-21 14:04:50 -0500

    generando modelos de datos

---
+++ src/app/models/info-pagina.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    instagram?: string;
+    twitter?: string;
+    tublr?: string;
+    equipo?: any[];
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../models/info-pagina';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
 
   constructor( private http: HttpClient ) {
 
     //leer el archivo json
     this.http.get('assets/data/data-pagina.json')
-    .subscribe( resp => {
+    .subscribe( (resp: InfoPagina )=> {
 
       this.cargada = true;
       this.info = resp;
